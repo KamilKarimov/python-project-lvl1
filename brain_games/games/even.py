714a4e1eb312866aa3ec9581467d937c3f2d7469
Камиль Каримов <chitatilo@gmail.com>
2022-07-16 22:08:02 +0400

    describe logic of even game

---
+++ brain_games/games/even.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+import prompt
+import random
+
+
+def is_even(number):
+    return number % 2 == 0
+
+
+def even_game():
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name?')
+    print(f'Hello, {name}!')
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+
+    
+    for _ in range(3):
+        counter = 0
+        while counter != 3:
+            number = random.randint(1, 100)
+            if is_even(number):
+                correct_answer = "yes"
+            else:
+                correct_answer = "no"
+            question = print(f'Question: {number}')
+            user_answer = prompt.string('Your answer: ')
+            if user_answer == correct_answer:
+                print('Correct!')
+                counter += 1
+            else:
+                print(f"{user_answer} is wrong answer ;(. Correct answer was {correct_answer}. Let's try again, {name}!")
+                break
+    print(f'Congratulations, {name}!')
