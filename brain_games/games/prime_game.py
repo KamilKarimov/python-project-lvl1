6ffa5c01325d560c220c5804ea60c2e6608b479f
Камиль Каримов <chitatilo@gmail.com>
2022-07-25 21:22:40 +0400

    fix prime_game and brain_prime due to adding engine file

--- brain_games/games/prime_game.py
+++ brain_games/games/prime_game.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import prompt
 import random
+
+TASK = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def is_prime(number):
@@ -18,27 +19,12 @@ def is_prime(number):
     return True
 
 
-def prime_game():
-    print('Welcome to the Brain Games!')
-    name = prompt.string('May I have your name?')
-    print(f'Hello, {name}!')
-    print('Answer "yes" if given number is prime. Otherwise answer "no".')
-    counter = 0
+def get_round():
     for _ in range(3):
         number = random.randint(2, 100)
+        question = number
         if is_prime(number):
             correct_answer = "yes"
         else:
             correct_answer = "no"
-        print(f'Question: {number}')
-        user_answer = prompt.string('Your answer: ')
-        if user_answer == correct_answer:
-            print('Correct!')
-            counter += 1
-        else:
-            print(f"'{user_answer}' is wrong answer ;(."
-                  f"Correct answer was '{correct_answer}'.")
-            print(f"Let's try again, {name}!")
-            return
-        if counter == 3:
-            print(f'Congratulations, {name}!')
+    return question, correct_answer
--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-from brain_games.games.prime_game import prime_game
+from brain_games.games import prime_game
+from brain_games.engine import run
 
 
 def main():
-    prime_game()
+    run(prime_game)
 
 
 if __name__ == '__main__':
