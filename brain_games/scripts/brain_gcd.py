9ee93f4e71475b84891a3a3e6d67fd5e67af4bf8
Камиль Каримов <chitatilo@gmail.com>
2022-07-25 21:21:37 +0400

    fix gcd_game and brain_gcd due to adding engine file

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-from brain_games.games.gcd_game import gcd_game
+from brain_games.games import gcd_game
+from brain_games.engine import run
 
 
 def main():
-    gcd_game()
+    run(gcd_game)
 
 
 if __name__ == '__main__':
