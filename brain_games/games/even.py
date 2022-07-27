284861032ca58204df02d906b38be7fadacf34a6
Камиль Каримов <chitatilo@gmail.com>
2022-07-27 19:58:59 +0400

    fix duplications

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -4,14 +4,14 @@ import random
 TASK = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
-def is_even(number):
-    return number % 2 == 0
+def is_even(num):
+    return num % 2 == 0
 
 
 def get_round():
-    number = random.randint(1, 100)
-    question = number
-    if is_even(number):
+    num = random.randint(1, 100)
+    question = num
+    if is_even(num):
         correct_answer = "yes"
     else:
         correct_answer = "no"
