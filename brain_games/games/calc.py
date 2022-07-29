c1bac217075064a0b6827a0d9c7eb0eac17203e8
Камиль Каримов <chitatilo@gmail.com>
2022-07-29 18:54:48 +0400

    fix calc game

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -2,11 +2,19 @@ import random
 
 TASK = 'What is the result of the expression?'
 
-
-def get_round():
+def get_correct_answer():
     number1 = random.randint(1, 100)
     number2 = random.randint(1, 100)
     exp = random.choice('+-*')
+    if exp == '+':
+        correct_answer = number1 + number2
+    elif exp == '-':
+        correct_answer = number1 - number2
+    else:
+        correct_answer = number1 * number2
+    return number1, number2, exp, correct_answer
+
+def get_round():
+    number1, number2, exp, correct_answer = get_correct_answer()
     question = f'{number1} {exp} {number2}'
-    correct_answer = eval(f'{number1} {exp} {number2}')
     return question, correct_answer
