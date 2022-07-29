4836f3dbe7be4900d3bb5b36c255c20a32cb04d1
Камиль Каримов <chitatilo@gmail.com>
2022-07-29 18:55:31 +0400

    fix progression game

--- brain_games/games/progression_game.py
+++ brain_games/games/progression_game.py
@@ -7,12 +7,9 @@ def get_round():
     num = random.randint(1, 10)
     progression = [(x * num) for x in range(1, 10)]
     random_index = random.randint(0, len(progression) - 1)
+    correct_answer = progression[random_index]
     progression[random_index] = '..'
     index = progression.index('..')
-    if index != 0:
-        correct_answer = progression[index - 1] + num
-    else:
-        correct_answer = progression[index + 1] - num
     question = ' '.join(
         '..' if num == correct_answer else str(num) for num in progression
     )
